4a0901a2681bf3be1082f29517a6d644a3c3fa7d
AKSHAY SACHAN <akshayshn@gmail.com>
2022-01-29 12:43:00 +0530

    client with rest endpoint

--- src/main/java/com/example/demo/SpringCloudEurekaClientAApplication.java
+++ src/main/java/com/example/demo/SpringCloudEurekaClientAApplication.java
@@ -1,18 +1,24 @@
 package com.example.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @EnableEurekaClient
 @SpringBootApplication
 @RestController
 @RefreshScope
+@RequestMapping("/clientA")
 public class SpringCloudEurekaClientAApplication {
+	
+    Logger logger = LoggerFactory.getLogger(SpringCloudEurekaClientAApplication.class);
 
 	@Value("${msg}")
 	private String msg;
@@ -21,8 +27,13 @@ public class SpringCloudEurekaClientAApplication {
 		SpringApplication.run(SpringCloudEurekaClientAApplication.class, args);
 	}
 
-	@GetMapping(value = "/")
+	@GetMapping(value = "/welcomeText")
 	public String welcomeText() {
+		try {
+			Thread.sleep(9000);
+		} catch (InterruptedException e) {
+			logger.error("Thread Interupted");
+		}
 		return msg;
 	}
 
